c89fe394debf0658c9d54242fe7f8859206d3113
Claudia CECCALDI <106426964+Claudia-Ceccaldi-BTSSIO@users.noreply.github.com>
2023-12-18 10:57:15 +0100

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -7,40 +7,25 @@ CREATE TABLE Utilisateurs (
     user_region VARCHAR(50)                         -- Région de l'utilisateur
 );
 
--- Table des administrateurs
+-- Création de la table des administrateurs
 CREATE TABLE Admins (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    utilisateur_id INT,
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+    id INT PRIMARY KEY AUTO_INCREMENT,              -- Identifiant unique pour chaque administrateur
+    utilisateur_id INT,                             -- Identifiant de l'utilisateur associé à l'administrateur
+    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)  -- Lien vers la table des utilisateurs
 );
 
--- Table des utilisateurs bannis
+-- Création de la table des utilisateurs bannis
 CREATE TABLE Bannis (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    admin_id INT,
-    utilisateur_id INT,
-    FOREIGN KEY (admin_id) REFERENCES Admins(id),
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+    id INT PRIMARY KEY AUTO_INCREMENT,              -- Identifiant unique pour chaque bannissement
+    admin_id INT,                                   -- Identifiant de l'administrateur qui a banni l'utilisateur
+    utilisateur_banni_id INT,                       -- Identifiant de l'utilisateur banni
+    FOREIGN KEY (admin_id) REFERENCES Admins(id),             -- Lien vers la table des administrateurs
+    FOREIGN KEY (utilisateur_banni_id) REFERENCES Utilisateurs(id)  -- Lien vers la table des utilisateurs
 );
-
--- Table des commentaires des livres
-CREATE TABLE CommentairesLivres (
+CREATE TABLE Commentaires (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    livre_id INT,
-    utilisateur_id INT,
+    id_utilisateur INT, -- Clé étrangère vers la table Utilisateurs
+    region VARCHAR(255),
     commentaire TEXT,
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (livre_id) REFERENCES Livres(id),
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE Livres (
-    id INT PRIMARY KEY AUTO_INCREMENT,        -- Identifiant unique pour chaque livre
-    titre VARCHAR(255) NOT NULL,             -- Titre du livre
-    auteur VARCHAR(255) NOT NULL,            -- Auteur du livre
-    description TEXT,                        -- Description ou résumé du livre
-    date_publication DATE,                   -- Date de publication
-    image_couverture VARCHAR(255),           -- Chemin vers l'image de couverture du livre
-    prix DECIMAL(10, 2)                      -- Prix du livre
-);
-
