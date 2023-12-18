aa9b14b98c0a48b4dee0a0c3ea4a5ac5305f600f
Claudia CECCALDI <106426964+Claudia-Ceccaldi-BTSSIO@users.noreply.github.com>
2023-12-18 20:31:26 +0100

    Update traitement_commentaire.php

--- traitement_commentaire.php
+++ traitement_commentaire.php
@@ -3,34 +3,38 @@
 require_once 'config.php';
 require_once 'session.php';
 
-// Récupère les données du formulaire
-$utilisateur_id = $_SESSION['utilisateur_id']; // Assurez-vous que cet ID est bien défini dans la session
-$commentaire = $_POST['commentaires']; // Récupération du commentaire
-
-// Préparation de la requête d'insertion
-$query = "INSERT INTO Commentaires (utilisateur_id, commentaire) VALUES (?, ?)";
-$stmt = $connexion->prepare($query);
-$stmt->bind_param("is", $utilisateur_id, $commentaire);
-
-//Vérification de utilisateur_id bien défini
-if (isset($_SESSION['utilisateur_id'])) {
-    $utilisateur_id = $_SESSION['utilisateur_id'];
-    // Le reste de votre script
-} else {
-    // Rediriger vers la page de connexion ou afficher un message d'erreur
+// Vérifie si l'utilisateur est connecté
+if (!isset($_SESSION['utilisateur_id'])) {
+    // Si l'utilisateur n'est pas connecté, redirige vers la page de connexion
     header("Location: connexion.php");
     exit;
 }
 
-// Exécution de la requête préparée
-if ($stmt->execute()) {
-    // Commentaire enregistré avec succès
-    header("Location: accueil.php"); // Rediriger l'utilisateur vers la page d'accueil
-} else {
-    // Échec de l'enregistrement du commentaire
-    echo 'Échec de l\'enregistrement du commentaire : ' . $stmt->error;
-}
+// Récupère l'ID de l'utilisateur depuis la session
+$utilisateur_id = $_SESSION['utilisateur_id'];
 
-$stmt->close();
-$connexion->close();
+// Récupère les données du formulaire
+if (isset($_POST['commentaire'])) {
+    $commentaire = $_POST['commentaire']; 
+
+    // Préparation de la requête d'insertion avec PDO
+    $query = "INSERT INTO CommentairesLivres (utilisateur_id, commentaire) VALUES (:utilisateur_id, :commentaire)";
+    $stmt = $maconnexion->prepare($query);
+
+    // Liaison des paramètres
+    $stmt->bindParam(":utilisateur_id", $utilisateur_id, PDO::PARAM_INT);
+    $stmt->bindParam(":commentaire", $commentaire, PDO::PARAM_STR);
+
+    // Exécution de la requête préparée
+    if ($stmt->execute()) {
+        // Commentaire enregistré avec succès, redirection vers la page d'accueil
+        header("Location: accueil.php");
+    } else {
+        // Échec de l'enregistrement du commentaire
+        echo 'Échec de l\'enregistrement du commentaire';
+    }
+} else {
+    // si aucun commentaire fourni, redirection vers la page d'accueil
+    header("Location: accueil.php");
+}
 ?>
