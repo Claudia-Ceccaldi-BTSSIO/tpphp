a314630723cdcbf0b0a15ea10ced3dda33c31cce
Claudia CECCALDI <106426964+Claudia-Ceccaldi-BTSSIO@users.noreply.github.com>
2023-12-18 10:54:15 +0100

    Update afficher_utilisateurs.php

--- afficher_utilisateurs.php
+++ afficher_utilisateurs.php
@@ -1,7 +1,7 @@
 <?php
 // Inclut le fichier de connexion à la base de données
 require_once 'config.php';
-require_once 'session.php';
+
 // Requête SQL pour récupérer tous les utilisateurs
 $query = "SELECT nom_utilisateur FROM Utilisateurs";
 $result = $connexion->query($query);
