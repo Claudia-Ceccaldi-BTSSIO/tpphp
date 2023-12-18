b5d8ed91970e2811ef01b2aa188e510148436b55
Claudia CECCALDI <106426964+Claudia-Ceccaldi-BTSSIO@users.noreply.github.com>
2023-12-18 10:58:25 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,8 +1,7 @@
 <?php
 // Inclut le fichier de configuration de la base de données
 require_once 'config.php';
-require_once 'session.php';
-session_start();
+
 // Message de succès lorsqu'un nouvel utilisateur est ajouté
 $succesMessage = "";
 $errorMessage = "";
@@ -34,132 +33,77 @@ $resultUtilisateurs = $connexion->query($queryUtilisateurs);
 $queryCommentaires = "SELECT * FROM Commentaires";
 $resultCommentaires = $connexion->query($queryCommentaires);
 ?>
+
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Librairie en Ligne</title>
+    <title>Page d'Accueil</title>
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-<?php if (isset($_SESSION['utilisateur_id'])) : ?>
-    <div class="user-greeting">
-        Bonjour, <?php echo htmlspecialchars($_SESSION['utilisateur_id']); ?>
-    </div>
-<?php endif; ?>
+    <!-- Formulaire d'ajout d'utilisateur -->
+    <h2>Ajouter un nouvel utilisateur</h2>
+    <form action="accueil.php" method="post">
+        <label for="nom_utilisateur">Nom d'utilisateur : </label>
+        <input type="text" name="nom_utilisateur" placeholder="Nom d'utilisateur" required>
+        <label for="motdepasse">Mot de passe : </label>
+        <input type="password" name="motdepasse" placeholder="Mot de passe" required>
+        <input type="submit" value="Ajouter">
+    </form>
+    <!-- Message de succès pour l'ajout d'un nouvel utilisateur -->
+    <?php if (!empty($succesMessage)) : ?>
+        <p class="success"><?php echo $succesMessage; ?></p>
+    <?php endif; ?>
+    <!-- Message d'erreur en cas d'échec de l'ajout -->
+    <?php if (!empty($errorMessage)) : ?>
+        <p class="error"><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
 
-    <style>
-body {
-    background-color: #f0f4f8;
-    color: #333;
-    font-family: 'Helvetica Neue', Arial, sans-serif;
-    text-align: center;
-}
+    <!-- Affichage des utilisateurs -->
+    <h2>Liste des utilisateurs</h2>
+    <ul>
+        <?php while ($rowUtilisateur = $resultUtilisateurs->fetch_assoc()) : ?>
+            <li><?php echo $rowUtilisateur['nom_utilisateur']; ?></li>
+        <?php endwhile; ?>
+    </ul>
 
-header {
-    background-color: #a7c7e7;
-    color: #333;
-    padding: 15px 0;
-}
+    <!-- Formulaire pour récupérer les commentaires et la région de l'utilisateur -->
+    <h2>Votre région</h2>
+    <form action="traitement_commentaires.php" method="post">
+        <label for="region">Région : </label>
+        <input type="text" name="region" placeholder="Entrez votre région" required>
+        
+        <h2>Vos commentaires</h2>
+        <label for="commentaires">Commentaires : </label>
+        <textarea name="commentaires" placeholder="Entrez vos commentaires ici"></textarea>
 
-.book-display {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    padding: 20px;
-}
+        <input type="submit" value="Envoyer">
+    </form>
 
-.book {
-    background-color: #e7eff6;
-    border: 1px solid #d0dae3;
-    color: #333;
-    margin: 10px;
-    padding: 20px;
-    width: 300px;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-}
+    <!-- Affichage des commentaires -->
+    <h2>Commentaires des utilisateurs</h2>
+    <?php if ($resultCommentaires->num_rows > 0) : ?>
+        <ul>
+            <?php while ($rowCommentaire = $resultCommentaires->fetch_assoc()) : ?>
+                <li>
+                    <p>Région : <?php echo $rowCommentaire['region']; ?></p>
+                    <p>Commentaire : <?php echo $rowCommentaire['commentaire']; ?></p>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php else : ?>
+        <p>Aucun commentaire trouvé.</p>
+    <?php endif; ?>
 
-.book img {
-    max-width: 100%;
-    height: auto;
-    margin-bottom: 15px;
-}
-
-.comment-section {
-    background-color: #d3d3d3;
-    padding: 10px;
-    margin-top: 15px;
-}
-
-footer {
-    background-color: #a7c7e7;
-    color: #333;
-    padding: 15px 0;
-}
-</style>
-    <header>
-        <h1>Bienvenue dans Notre Librairie</h1>
-    </header>
-
-    <section class="book-display">
-        <!-- Livre 1 -->
-        <article class="book">
-            <img src="images/vent.jpg" alt="Couverture du livre 1">
-            <h2>Vent d'Est , vent d'Ouest</h2>
-            <p>Le livre Vent d’Est, vent d’Ouest de Pearl Buck raconte l’histoire de Kwei-Lan, une jeune fille chinoise qui vient d’être mariée à un homme de sa race qui revient d’Europe. Ce dernier a oublié la loi des ancêtres et ne respecte plus les coutumes ni les rites. Le frère de Kwei-Lan, qui est l’héritier mâle, annonce son mariage avec une étrangère. A travers les réactions 
-                de cette famille, le roman aborde les thèmes de l’amour, de la tolérance et de l’apprentissage1</p>
-            <div class="comment-section">
-            <form action="traitement_commentaire.php" method="post">
-            <input type="hidden" name="book_id" value="01"> <!-- Remplacer par l'ID réel du livre -->
-            <textarea name="comment" placeholder="Votre commentaire"></textarea>
-            <input type="submit" value="Envoyer le commentaire">
-        </form>
-            </div>
-        </article>
-
-        <!-- Livre 2 -->
-        <article class="book">
-            <img src="images/terre.jpg" alt="Couverture du livre 2">
-            <h2>La terre chinoise</h2>
-            <p>Ce roman raconte l’histoire de Wang Lung, un agriculteur chinois qui vit dans un village avant la Première Guerre mondiale. Le récit suit sa vie de famille et les épreuves qu’il traverse pour acquérir une fortune considérable. 
-                Le livre a été publié en 1931 et a remporté le Prix Pulitzer de la fiction en 1932.</p>
-            <div class="comment-section">
-            <form action="traitement_commentaire.php" method="post">
-            <input type="hidden" name="book_id" value="ID_DU_LIVRE_1"> <!-- Remplacer par l'ID réel du livre -->
-            <textarea name="comment" placeholder="Votre commentaire"></textarea>
-            <input type="submit" value="Envoyer le commentaire">
-            </form>
-            </div>
-        </article>
-
-        <!-- Livre 3 -->
-        <article class="book">
-            <img src="images/dragon.jpg" alt="Couverture du livre 3">
-            <h2>Le fils du dragon</h2>
-            <p></p>
-            <div class="comment-section">
-            <form action="traitement_commentaire.php" method="post">
-            <input type="hidden" name="book_id" value="ID_DU_LIVRE_1"> <!-- Remplacer par l'ID réel du livre -->
-            <textarea name="comment" placeholder="Votre commentaire"></textarea>
-            <input type="submit" value="Envoyer le commentaire">
-            </form>
-            </div>
-        </article>
-    </section>
-    <nav>
-        <!-- lien déconnexion -->
-        <a href="logout.php">Déconnexion</a>
-    </nav>
-    <footer>
-        <p>&copy; 2023 Librairie en Ligne</p>
-    </footer>
 </body>
 </html>
 
 <?php
 // Ferme les résultats des requêtes
 $resultUtilisateurs->close();
+$resultCommentaires->close();
 
 // Ferme la connexion à la base de données 
 $connexion->close();
